e23a7f929d361bc458692a71085d426c2e6e9942
Royden Rego <roydenrego@softrixz.com>
2018-10-07 11:03:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/roydenrego/imageshare/GenericFileProvider.java
@@ -0,0 +1,6 @@
+package com.roydenrego.imageshare;
+
+import android.support.v4.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+}
---
+++ app/src/main/java/com/roydenrego/imageshare/MainActivity.java
@@ -0,0 +1,72 @@
+package com.roydenrego.imageshare;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent data = getIntent();
+
+        if(data != null && data.getExtras() != null) {
+
+            String url = data.getExtras().getString("android.intent.extra.TEXT");
+            Log.i("test", data.getExtras().getString("android.intent.extra.SUBJECT"));
+
+            Picasso.get().load(url).into(new Target() {
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("image/*");
+
+                    Uri photoURI = FileProvider.getUriForFile(MainActivity.this, getApplicationContext().getPackageName() + ".com.roydenrego.imageshare.provider", getLocalFile(bitmap));
+
+                    i.putExtra(Intent.EXTRA_STREAM, photoURI);
+                    i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(Intent.createChooser(i, "Share Image"));
+                }
+
+                @Override
+                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+                }
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                }
+            });
+        }
+    }
+
+    public File getLocalFile(Bitmap bmp) {
+        File file = null;
+        try {
+            file =  new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+}
